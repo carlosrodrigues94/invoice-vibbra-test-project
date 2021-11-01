47912052b0cbe6fb46b9ae3645148c34d7b0955c
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-11-01 17:19:16 -0300

    feat: types

---
+++ src/types/category.ts
@@ -0,0 +1,6 @@
+export type ICategory = {
+  name: string;
+  description: string;
+  id: string;
+  isArchived: boolean;
+};
---
+++ src/types/company.ts
@@ -0,0 +1,7 @@
+export type ICompany = {
+  id: string;
+  cnpj: string;
+  socialName: string;
+  name: string;
+  createdAt: Date;
+};
